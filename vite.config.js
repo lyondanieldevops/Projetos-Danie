56f28ba506ba0662da2a9f5d21f9133100b5a491
lyon daniel <lyondanieldevops@gmail.com>
2026-01-20 18:09:22 -0300

    Ajuste do base path para ./

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/site-daniel-lesson/',
+  base: './', // Isso faz com que o site procure os arquivos na pasta atual, sem erro
 })
